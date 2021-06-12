abf7315f0744c5e567e498cae052afc47230fe08
jacarni <67039652+jacarni@users.noreply.github.com>
2021-06-12 17:34:17 -0400

    Add files via upload

---
+++ list_tcp_ports.bat
@@ -0,0 +1,13 @@
+@echo off
+netstat -anb
+
+@echo on
+ >"C:\Users\0_0\Desktop\netstat_output.txt"
+
+@echo on
+@echo TCP port successfully captured.
+@echo
+@echo File created at C:\netstat_output.txt
+
+@echo off
+pause
